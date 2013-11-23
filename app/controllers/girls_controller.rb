b6c0ef076e2914d526b9384550d6f324aac77038
moralesalberto <alberto@moralitos.com>
2013-11-23 02:23:23 -0500

    adding the rescue for missing image

--- app/controllers/girls_controller.rb
+++ app/controllers/girls_controller.rb
@@ -6,7 +6,11 @@ class GirlsController < ApplicationController
   end
 
   def avatar
-    @girl = Girl.where(:product_code => params[:product_code]).first
-    send_file @girl.avatar_file_path, :disposition => 'inline'
+    begin
+      @girl = Girl.where(:product_code => params[:product_code]).first
+      send_file @girl.avatar_file_path, :disposition => 'inline'
+    rescue
+      send_file "#{Rails.root}/app/assets/images/avatar.jpg", :disposition => 'inline'
+    end
   end
 end
